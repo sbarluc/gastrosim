28c719c2bc9ec75d18c24514a1239215f2796c21
sbarluc <sbarretoluc@gmail.com>
2026-06-09 03:43:59 -0300

    Implementa agregar y quitar objetos y testea

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+from models.objeto import Objeto
 from models.mesa import Mesa
 from models.cliente import Cliente
 
 mesa = Mesa(1, 2)
 
+servilletero = Objeto("Servilletero")
+vaso_sucio = Objeto("Vaso sucio")
+
+mesa.ensuciar()
+mesa.agregar_objeto(servilletero)
+mesa.agregar_objeto(vaso_sucio)
+
 ana = Cliente("Ana", 30)
-pedro = Cliente("Pedro", 25)
 
 mesa.sentar_cliente(ana)
-mesa.sentar_cliente(pedro)
+
+print(mesa)
+
+mesa.quitar_objeto(vaso_sucio)
+mesa.limpiar()
 
 print(mesa)
