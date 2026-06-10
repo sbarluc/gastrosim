e9fc4b9f8fd44dc0dbd22e565ee7cc7d4b865ddd
sbarreto10 <sbarreto@fi.uba.ar>
2026-06-09 22:31:20 -0300

    Implementa Empleado.cargar_objeto y Empleado.dejar_objeto

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from models.objeto import Objeto
 from models.mesa import Mesa
 from models.cliente import Cliente
-
-mesa = Mesa(1, 2)
+from models.empleado import Empleado
 
 servilletero = Objeto("Servilletero")
-lata = Objeto("Lata")
-
-mesa.agregar_objeto(servilletero)
+mesa = Mesa(1, 2)
+seve = Empleado("Seve", "Moz@")
 maxi = Cliente("Maxi", 25)
 maxi.agregar_caracteristica("enojad@")
 
+seve.cargar_objeto(servilletero)
+
+print(seve)
 print(mesa)
 print(f"{maxi}\n")
 
 mesa.sentar_cliente(maxi)
-mesa.agregar_objeto(lata)
-mesa.ensuciar()
+seve.dejar_objeto(mesa, servilletero)
 
+print(seve)
 print(mesa)
 print(f"{maxi}\n")
--- models/empleado.py
+++ models/empleado.py
@@ -1,6 +1,32 @@
 class Empleado:
+
     def __init__(self, nombre, puesto):
+
         self.nombre = nombre
         self.puesto = puesto
 
+        self.estado = "libre"
+        self.posicion = None
+
         self.inventario = []
+
+    def cargar_objeto(self, objeto):
+        self.inventario.append(objeto)
+
+    def dejar_objeto(self, mesa, objeto):
+        if objeto not in self.inventario:
+            return False
+
+        self.inventario.remove(objeto)
+        mesa.agregar_objeto(objeto)
+
+        return True
+
+    def __repr__(self):
+        return (
+            f"Empleado("
+            f"nombre='{self.nombre}', "
+            f"puesto='{self.puesto}', "
+            f"objetos={len(self.inventario)}"
+            f")"
+        )
