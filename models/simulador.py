9abddcc58ddf7ff6869933520ce734e61e3e5d41
sbarreto10 <sbarreto@fi.uba.ar>
2026-06-26 01:32:30 -0300

    Reimplementa entidades de la simulacion como diccionario de diccionarios y Modifica como se muestran

--- models/simulador.py
+++ models/simulador.py
@@ -1,35 +1,31 @@
 class Simulador:
 
     def __init__(self):
-        self._mesas = []
-        self._estanterias = []
-        self._empleados = []
-        self._clientes = []
-
-        self._entidades = {}
+        self._entidades = {
+            "mesa": {},
+            "empleado": {},
+            "cliente": {},
+            "estanteria": {}
+        }
 
     def agregar_mesas(self, mesas):
         for mesa in mesas:
-            self._mesas.append(mesa)
-            self._entidades[mesa.id_gral] = mesa
+            self._entidades["mesa"][mesa.id] = mesa
         return self
 
     def agregar_empleados(self, empleados):
         for empleado in empleados:
-            self._empleados.append(empleado)
-            self._entidades[empleado.id_gral] = empleado
+            self._entidades["empleado"][empleado.id] = empleado
         return self
 
     def agregar_clientes(self, clientes):
         for cliente in clientes:
-            self._clientes.append(cliente)
-            self._entidades[cliente.id_gral] = cliente
+            self._entidades["cliente"][cliente.id] = cliente
         return self
 
     def agregar_estanterias(self, estanterias):
         for estanteria in estanterias:
-            self._estanterias.append(estanteria)
-            self._entidades[estanteria.id_gral] = estanteria
+            self._entidades["estanteria"][estanteria.id] = estanteria
         return self
 
     def buscar_entidad(self, id_gral):
@@ -38,22 +34,22 @@ class Simulador:
         return self._entidades[id_gral]
     
     def mostrar_universo(self):
-        for mesa in self._mesas:
-            salida = f"Mesa{mesa.id}" + \
+        for id_mesa, mesa in self._entidades["mesa"].items():
+            print(f"Mesa{id_mesa}" + \
             (f" <sentados: {mesa.clientes_sentados()}>" if mesa.esta_ocupada() else "") + \
-            (f" <objetos: {mesa.objetos()}>" if len(mesa.objetos())>0 else "")
-            print(salida)
+            (f" <objetos: {mesa.objetos()}>" if len(mesa.objetos())>0 else ""))
 
-        for estanteria in self._estanterias:
-            print(f"Estanteria{estanteria.id}")
+        for id_cliente, cliente in self._entidades["cliente"].items():
+            print(f"Cliente{id_cliente} <nombre: {cliente.nombre}>" + \
+            (f" <mesa asignada: {cliente.mesa_asignada()}>" if cliente.mesa_asignada() is not None else "") + \
+            (f" <sentad@ en: {cliente.mesa_actual()}>" if cliente.mesa_actual() is not None else ""))
 
-        for cliente in self._clientes:
-            salida = f"Cliente{cliente.id} <nombre: {cliente.nombre}>" + \
-            (f" <mesa actual: {cliente.mesa_actual()}>" if cliente.mesa_actual() is not None else "") + \
-            (f" <mesa asignada: {cliente.mesa_asignada()}>" if cliente.mesa_asignada() is not None else "")
-            print(salida)
-
-        for empleado in self._empleados:
-            print(f"Empleado{empleado.id} <nombre: {empleado.nombre}> <puesto: {empleado.puesto}>")
-        
+        for id_empleado, empleado in self._entidades["empleado"].items():
+            print(f"Empleado{id_empleado}" + \
+            (f" <nombre: {empleado.nombre}> <puesto: {empleado.puesto}>") + \
+            (f" <objetos: {empleado.inventario().objetos()}>" if len(empleado.inventario().objetos())>0 else ""))
+            
+        for id_estanteria, estanteria in self._entidades["estanteria"].items():
+            print(f"Estanteria{id_estanteria}" + \
+            (f" <objetos: {estanteria.objetos()}>" if len(estanteria.objetos())>0 else ""))
         
