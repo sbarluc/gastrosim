1fc71c90b33d3505b0cdcd7ef665b6d06fd62edf
sbarreto10 <sbarreto@fi.uba.ar>
2026-06-27 13:04:18 -0300

    data: añade data de precios

---
+++ data/test_menu.py
@@ -0,0 +1,18 @@
+def dicc_precios():
+    return {
+        "Cafe con leche": 3000, 
+        "Croissant nutella": 4000, 
+        "Cheesecake frutos rojos": 5000,
+        "Milanesa con papas fritas": 8500,
+        "Pollo al horno con ensalada": 7800,
+        "Lomo saltado": 9200,
+        "Ravioles con salsa fileto": 7200,
+        "Ensalada César con pollo": 6800,
+        "Sándwich de miga completo": 5500,
+        "Pizza margarita": 7500,
+        "Hamburguesa con cheddar": 6500,
+        "Tarta de espinaca": 5800,
+        "Salmón a la plancha": 9800,
+        "Ñoquis con pesto": 6900,
+        "Risotto de hongos": 8200
+    } 
