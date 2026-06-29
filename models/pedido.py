194524faf09b3daa69019044edd04280747ca98c
sbarreto10 <sbarreto@fi.uba.ar>
2026-06-29 13:04:51 -0300

    fix(Pedido): cambia forma de eliminacion de items y añade utiles

--- models/pedido.py
+++ models/pedido.py
@@ -5,14 +5,8 @@ class Pedido():
     def __init__(self, mesa, menu=None):
         self.menu = menu
         self._mesa_actual = mesa
-        self.precio = 0
+        self._precio = 0
         self._items = []
-
-    def _buscar_item(self, nombre):
-        for item in self._items:
-            if item.nombre == nombre:
-                return item
-        return None
 
     def cantidad_items(self):
         return len(self._items)
@@ -21,20 +15,30 @@ class Pedido():
         item = ItemPedido(self.menu, nombre)
         if item is not None:
             self._items.append(item)
-            self.precio += item.precio
+            self._precio += item.precio()
             return True
         return False
     
     def quitar_item(self, nombre):
-        item = self._buscar_item(nombre)
+        item = self._buscar_item_no_entregado(nombre)
         if item is not None:
             self._items.remove(item)
-            self.precio -= item.precio
+            self._precio -= item.precio()
             return True
         return False
     
     def mesa_actual(self):
         return self._mesa_actual
+    
+    def precio(self):
+        return self._precio
 
     def cambiar_mesa(self, mesa):
         self._mesa_actual = mesa
+
+    def _buscar_item_no_entregado(self, nombre):
+        return next(
+            (item for item in self._items 
+             if (item.nombre == nombre and not item.fue_entregado()))
+            , None
+        )
