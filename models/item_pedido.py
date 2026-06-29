9eb1fc3ef745509d13a817c9c4f2e6fda58f083d
sbarreto10 <sbarreto@fi.uba.ar>
2026-06-29 13:03:42 -0300

    feat(ItemPedido): añade atributos y getters

--- models/item_pedido.py
+++ models/item_pedido.py
@@ -1,5 +1,14 @@
-class ItemPedido():
+from models.entidad import Entidad
+
+class ItemPedido(Entidad):
     
     def __init__(self, menu, nombre):
         self.nombre = nombre
-        self.precio = menu.precio(nombre)
+        self._precio = menu.precio(nombre)
+        self._entregado = False
+
+    def fue_entregado(self):
+        return self._entregado
+    
+    def precio(self):
+        return self._precio
