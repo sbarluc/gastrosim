382bfe23a91e43cf86684141e3066b12ab222dcc
sbarreto10 <sbarreto@fi.uba.ar>
2026-06-29 13:05:18 -0300

    test(ItemPedido y Pedido): testea reimplementación

--- tests/test_pedidos.py
+++ tests/test_pedidos.py
@@ -11,13 +11,13 @@ def test_menu_recibe_diccionario_de_precios():
 def test_crear_item_desde_un_menu():
     menu = Menu(dicc_precios)
     item_pedido = ItemPedido(menu, "Croissant nutella")
-    assert item_pedido.precio == 4000
+    assert item_pedido.precio() == 4000
 
 def test_pedido_para_mesa_comienza_vacio():
     mesa = Mesa(1)
     pedido = Pedido(mesa)
     assert pedido.cantidad_items() == 0
-    assert pedido.precio == 0
+    assert pedido.precio() == 0
     assert pedido.mesa_actual() == mesa
 
 def test_agregar_item_a_pedido_actualiza_el_precio_total():
@@ -26,7 +26,7 @@ def test_agregar_item_a_pedido_actualiza_el_precio_total():
     pedido = Pedido(mesa, menu)
     pedido.agregar_item("Salmón a la plancha")
     assert pedido.cantidad_items() == 1
-    assert pedido.precio == 9800
+    assert pedido.precio() == 9800
 
 def test_quitar_item_a_pedido_actualiza_el_precio_total():
     menu = Menu(dicc_precios)
@@ -35,7 +35,7 @@ def test_quitar_item_a_pedido_actualiza_el_precio_total():
     pedido.agregar_item("Cafe con leche")
     pedido.agregar_item("Croissant nutella")
     pedido.quitar_item("Croissant nutella")
-    assert pedido.precio == 3000
+    assert pedido.precio() == 3000
 
 def test_pedido_cambia_de_mesa():
     mesa1 = Mesa(1)
