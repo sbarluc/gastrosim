a7151a118b20573c690ce2fe2b79f262c996c3ca
sbarreto10 <sbarreto@fi.uba.ar>
2026-06-30 14:38:57 -0300

    test(pedidos y menu): ItemPedido

--- tests/test_pedidos_y_menu.py
+++ tests/test_pedidos_y_menu.py
@@ -30,3 +30,33 @@ def test_menu_precio_inexistente(menu):
 
 
 
+def test_item_pedido_se_crea_desde_menu(menu):
+    item = ItemPedido(menu, "Cafe con leche")
+    assert item.nombre == "Cafe con leche"
+    assert item.valor() == 3000
+
+    item2 = ItemPedido(menu, "Croissant nutella")
+    assert item2.nombre == "Croissant nutella"
+    assert item2.valor() == 4000
+
+def test_item_pedido_con_nombre_inexistente(menu):
+    item = ItemPedido(menu, "Plato inexistente")
+    assert item.nombre == "Plato inexistente"
+    assert item.valor() == None
+
+def test_item_pedido_sin_menu():
+    item = ItemPedido()
+    assert item.nombre == ""
+    assert item.valor() == 0
+
+
+def test_item_pedido_entregar(menu):
+    item = ItemPedido(menu, "Cafe con leche")
+    assert item.fue_entregado() is False
+    item.entregar()
+    assert item.fue_entregado() is True
+
+def test_item_pedido_descuento(menu):
+    item_descuento = ItemPedido().descuento(2000)
+    assert item_descuento.nombre == "Descuento"
+    assert item_descuento.valor() == 2000
