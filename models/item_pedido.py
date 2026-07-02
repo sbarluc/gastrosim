5f1e642f184cc28aa53f1ed49439bd81beeac2a2
Seve <sbarretoluc@gmail.com>
2026-07-02 20:09:25 -0300

    feat: actualiza item_pedido.py

--- models/item_pedido.py
+++ models/item_pedido.py
@@ -6,15 +6,22 @@ class ItemPedido(Entidad):
         self.nombre = nombre
         self._valor = valor
         self._entregado = False
+        self._fue_pedido = False
 
     def fue_entregado(self):
         return self._entregado
+
+    def fue_pedido(self):
+        return self._fue_pedido
     
     def valor(self):
         return self._valor
     
     def entregar(self):
         self._entregado = True
+
+    def pedir(self):
+        self._fue_pedido = True
 
     @classmethod
     def desde_menu(cls, menu, nombre):
