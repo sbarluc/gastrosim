f9a1e41364d7c735b608c9b96e4104c821d3736e
sbarreto10 <sbarreto@fi.uba.ar>
2026-07-03 14:25:42 -0300

    feat(ItemPedido): reimplementa estados como un set

--- models/item_pedido.py
+++ models/item_pedido.py
@@ -5,23 +5,34 @@ class ItemPedido(Entidad):
     def __init__(self, nombre="", valor=0):
         self.nombre = nombre
         self._valor = valor
-        self._entregado = False
-        self._fue_pedido = False
+        self._estados = set()
 
-    def fue_entregado(self):
-        return self._entregado
+#------------------------------------------------------------------------------------
 
-    def fue_pedido(self):
-        return self._fue_pedido
-    
     def valor(self):
         return self._valor
     
-    def entregar(self):
-        self._entregado = True
-
+    def preparar_para_pedir(self):
+        return self._estados.add("PARA_PEDIR")
+    
+    def esta_para_pedir(self):
+        return "PARA_PEDIR" in self._estados
+    
     def pedir(self):
-        self._fue_pedido = True
+        return self._estados.add("ESPERANDO")
+
+    def fue_pedido(self):
+        return "ESPERANDO" in self._estados
+    
+#-------------------------------------------------------------------------------------
+    
+    def entregar(self):
+        return self._estados.add("ENTREGADO")
+    
+    def fue_entregado(self):
+        return "ENTREGADO" in self._estados
+
+#-------------------------------------------------------------------------------------
 
     @classmethod
     def desde_menu(cls, menu, nombre):
