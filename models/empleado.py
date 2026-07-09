a9bf7b2d116ecf0e6c8647cd0051aac9535daa9f
sbarreto10 <sbarreto@fi.uba.ar>
2026-07-09 03:35:10 -0300

    feat: reimplementa clase Empleado

--- models/empleado.py
+++ models/empleado.py
@@ -11,34 +11,12 @@ class Empleado(Entidad):
         self.nombre = nombre
         self.puesto = puesto
 
-        self._estado = "libre"
-        self._posicion = None
         self._inventario = Inventario()
         self._pedidos = {}
 
-    def cargar_objeto(self, origen, objeto):
-        objeto = origen.quitar_objeto(objeto)
-        if objeto is None:
-            return False
-        self._inventario.agregar_objeto(objeto)
-        return True
-
-    def dejar_objeto(self, destino, objeto):
-        objeto = self._inventario.quitar_objeto(objeto)
-        if objeto is None:
-            return False
-        destino.agregar_objeto(objeto)
-        return True
-
-    def asignar_mesa_a_cliente(self, cliente, mesa):
-        cliente.asignar_mesa(mesa)
-
-    def desasignar_mesa_a_cliente(self, cliente, mesa):
-        cliente.desasignar_mesa(mesa)
-
     def inventario(self):
-        return self._inventario.copy()
-
+        return self._inventario
+    
     def crear_pedido(self, mesa):
         if mesa and not mesa.id in self._pedidos:
             pedido = Pedido(mesa)
@@ -46,53 +24,19 @@ class Empleado(Entidad):
                 self._pedidos[mesa.id] = Pedido(mesa)
                 return True
         return False
-
-    def agregar_item_a_pedido(self, mesa, item):
-        if mesa.id in self._pedidos:
-            return self._pedidos[mesa.id].agregar_item(item)
-        return False
     
-    def quitar_item_a_pedido(self, mesa, item):
-        if mesa.id in self._pedidos:
-            return self._pedidos[mesa.id].quitar_item(item)
-        return False
-    
-    def limpiar_items_de_pedido(self, mesa):
-        if mesa.id in self._pedidos:
-            return self._pedidos[mesa.id].limpiar_items()
-        return False
-
-    def tomar_pedido_a_mesa(self, mesa):
-        pedido_mesa = self.pedido_de_mesa(mesa)
-        if pedido_mesa is None:
-            return False
-        
-        for cliente in mesa.clientes_sentados():
-            for item in cliente.items():
-                if item.esta_para_pedir():
-                    if pedido_mesa.agregar_item(item):
-                        item.pedir()
-        
-        return True
-
-    def cerrar_pedido(self, mesa):
-        if mesa.id in self._pedidos:
-            del self._pedidos[mesa.id]
-            return True
-        return False
-
-    def pedido_de_mesa(self, mesa):
-        if mesa.id in self._pedidos:
-            return self._pedidos[mesa.id]
-        return None
-
     def cantidad_pedidos(self):
         return len(self._pedidos)
     
     def pedidos(self):
         return self._pedidos.copy()
 
-    def total_pedidos(self):
+    def pedido_de_mesa(self, mesa):
+        if mesa.id in self._pedidos:
+            return self._pedidos[mesa.id]
+        return None
+
+    def valor_total_pedidos(self):
         return sum([pedido.valor() for id, pedido in self._pedidos.items()])
 
     def resumen_pedido(self, mesa):
@@ -104,12 +48,6 @@ class Empleado(Entidad):
             }
         return None
 
-    def entregar_item_de_pedido(self, mesa, item):
-        if mesa.id in self._pedidos:
-            item = self._pedidos[mesa.id].buscar_item(item)
-            if item:
-                item.entregar()
-
     def info(self):
         pedidos = ""
         for id in self._pedidos:
@@ -119,3 +57,70 @@ class Empleado(Entidad):
             f"Inventario: {self._inventario.info()}" + \
             pedidos
         )
+    
+    
+    
+    
+    # ### OUTDATED
+    # def cargar_objeto(self, origen, objeto):
+    #     objeto = origen.quitar_objeto(objeto)
+    #     if objeto is None:
+    #         return False
+    #     self._inventario.agregar_objeto(objeto)
+    #     return True
+    # ### OUTDATED
+    # def dejar_objeto(self, destino, objeto):
+    #     objeto = self._inventario.quitar_objeto(objeto)
+    #     if objeto is None:
+    #         return False
+    #     destino.agregar_objeto(objeto)
+    #     return True
+    # ### OUTDATED
+    # def asignar_mesa_a_cliente(self, cliente, mesa):
+    #     cliente.asignar_mesa(mesa)
+    # ### OUTDATED
+    # def desasignar_mesa_a_cliente(self, cliente, mesa):
+    #     cliente.desasignar_mesa(mesa)
+    # ### OUTDATED
+
+    # ### OUTDATED
+    # def agregar_item_a_pedido(self, mesa, item):
+    #     if mesa.id in self._pedidos:
+    #         return self._pedidos[mesa.id].agregar_item(item)
+    #     return False
+    # ### OUTDATED
+    # def quitar_item_a_pedido(self, mesa, item):
+    #     if mesa.id in self._pedidos:
+    #         return self._pedidos[mesa.id].quitar_item(item)
+    #     return False
+    # ### OUTDATED
+    # def limpiar_items_de_pedido(self, mesa):
+    #     if mesa.id in self._pedidos:
+    #         return self._pedidos[mesa.id].limpiar_items()
+    #     return False
+    # ### OUTDATED
+    # def tomar_pedido_a_mesa(self, mesa):
+    #     pedido_mesa = self.pedido_de_mesa(mesa)
+    #     if pedido_mesa is None:
+    #         return False
+        
+    #     for cliente in mesa.clientes_sentados():
+    #         for item in cliente.items():
+    #             if item.esta_para_pedir():
+    #                 if pedido_mesa.agregar_item(item):
+    #                     item.pedir()
+        
+    #     return True
+    # ### OUTDATED
+    # def cerrar_pedido(self, mesa):
+    #     if mesa.id in self._pedidos:
+    #         del self._pedidos[mesa.id]
+    #         return True
+    #     return False
+    # ### OUTDATED
+    # def entregar_item_de_pedido(self, mesa, item):
+    #     if mesa.id in self._pedidos:
+    #         item = self._pedidos[mesa.id].buscar_item(item)
+    #         if item:
+    #             item.entregar()
+    # ### OUTDATED
